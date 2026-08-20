ef6a435b995fbcd4cf5214788b4932a50355dd89
KIKI DANIEL BRYAN <ngdani116@gmail.com>
2026-08-21 00:00:09 +0100

    feat: ajout du dossier gedii-backend (projet Spring Boot initialisé)

---
+++ gedii-backend/gedii-backend/src/test/java/com/mincom/gedii/gediibackend/GediiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mincom.gedii.gediibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GediiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
